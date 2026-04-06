c0be6ba273a9c4bb8dfac43d2b641a0e9f51d35e
Bekir Gulestan <gulestanbekir@gmail.com>
2026-04-06 12:38:00 +0200

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import {SignIn, SignInButton} from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-        test Home Page
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center  py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h1>Welcome to Pulth!</h1>
+        <div>
+            <Link href={"/quiz"}>Quizzes</Link>
+            <Link href={"/quiz/"}>CreateQuiz</Link>
+        </div>
+
+      </main>
     </div>
   );
 }
