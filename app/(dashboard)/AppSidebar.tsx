bc86c931642582516f29dfcdbbb085560c93ce2b
Bekir Gulestan <gulestanbekir@gmail.com>
2026-04-07 14:36:18 +0200

    feat: initial commit

---
+++ app/(dashboard)/AppSidebar.tsx
@@ -0,0 +1,18 @@
+export function AppSidebar() {
+    return (
+        <pnpm dlx shadcn@latest add dropdown-menuSidebar collapsible="icon" {...props}>
+            <SidebarHeader>
+                <TeamSwitcher teams={data.teams} />
+            </SidebarHeader>
+            <SidebarContent>
+                <NavMain items={data.navMain} />
+                <NavProjects projects={data.projects} />
+            </SidebarContent>
+            <SidebarFooter>
+                <NavUser user={data.user} />
+            </SidebarFooter>
+            <SidebarRail />
+        </Sidebar>
+    )
+}
+
