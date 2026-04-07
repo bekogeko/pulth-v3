841b1c1cd7ae08273a3f31522a1b7a3fb01d0114
Bekir Gülestan <gulestanbekir@gmail.com>
2026-04-07 11:31:43 +0200

    Add Sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,30 @@
 import Image from "next/image";
-import {SignIn, SignInButton} from "@clerk/nextjs";
+import {Show, SignIn, SignInButton, SignUpButton, UserButton} from "@clerk/nextjs";
 import Link from "next/link";
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center  py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1>Welcome to Pulth!</h1>
-        <div>
-            <Link href={"/quiz"}>Quizzes</Link>
-            <Link href={"/quiz/"}>CreateQuiz</Link>
-        </div>
 
+          <Show when="signed-out">
+              <SignInButton>
+                  <Button variant={"link"}>Sign in</Button>
+              </SignInButton>
+              <SignUpButton>
+                <Button variant={"link"}>
+                    Sign Up
+                </Button>
+              </SignUpButton>
+          </Show>
+          <Show when="signed-in">
+              <UserButton />
+              <Button variant={"link"}>
+                  <Link href="/quiz"> Quizzes</Link>
+              </Button>
+          </Show>
       </main>
     </div>
   );
