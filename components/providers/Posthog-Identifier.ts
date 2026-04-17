827c0f41235ffdb744871253d2660b24427919d5
Bekir Gülestan <gulestanbekir@gmail.com>
2026-04-17 13:06:33 +0200

    feat: Create client-side PostHog user identification component

---
+++ components/providers/Posthog-Identifier.ts
@@ -0,0 +1,19 @@
+"use client"
+import {useEffect} from "react";
+import {useAuth, useUser} from "@clerk/nextjs";
+import posthog from "posthog-js";
+
+export default function PosthogIdentifier(){
+
+    const {isSignedIn,userId} = useAuth();
+    const {user}=useUser();
+
+    useEffect(()=>{
+        if(isSignedIn && userId && user && !posthog._isIdentified()){
+            posthog.identify(userId,{
+                email:user.primaryEmailAddress,
+            });
+        }
+    },[userId,user,isSignedIn]);
+    return null;
+}
