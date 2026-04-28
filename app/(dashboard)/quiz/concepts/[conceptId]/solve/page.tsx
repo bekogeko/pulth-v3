986922d7a12edf42a8f7fa57be3fa38a40da7daf
Bekir Gülestan <gulestanbekir@gmail.com>
2026-04-28 11:25:38 +0200

    add generateMetadata for concept quiz

--- app/(dashboard)/quiz/concepts/[conceptId]/solve/page.tsx
+++ app/(dashboard)/quiz/concepts/[conceptId]/solve/page.tsx
@@ -2,7 +2,23 @@ import {dehydrate, HydrationBoundary, QueryClient} from "@tanstack/react-query";
 import {notFound, permanentRedirect} from "next/navigation";
 
 import {QuizSolver} from "@/app/(dashboard)/quiz/[slug]/solve/QuizSolver";
-import {getConceptByIdentifier, getQuestionsByConceptId} from "@/app/(dashboard)/quiz/quiz";
+import {getConceptByIdentifier, getQuestionsByConceptId, getQuizBySlug} from "@/app/(dashboard)/quiz/quiz";
+import {Metadata} from "next";
+
+
+export async function generateMetadata(
+    { params }: {params:Promise<{ conceptId: string }>}): Promise<Metadata> {
+    // read route params
+    const { conceptId } = await params
+    
+    // fetch data
+    const concept = await getConceptByIdentifier(conceptId).then((result)=>result[0]);
+
+    return {
+        title: concept.name,
+        description: concept.description,
+    }
+}
 
 export default async function SolveConceptPage({params}: {
     params: Promise<{ conceptId: string }>
