682fe92e1f5ca5d1ebe036b4f4223617643790bd
Bekir Gülestan <gulestanbekir@gmail.com>
2026-05-11 17:21:50 +0200

    database pooling with max argument

--- lib/database.ts
+++ lib/database.ts
@@ -3,6 +3,7 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 export const database = drizzle({
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: true
-    }
+        max: Number(process.env.DATABASE_POOL_MAX ?? 1),
+        ssl: true,
+    },
 });
