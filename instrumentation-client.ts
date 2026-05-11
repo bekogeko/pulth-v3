a832922d96affe9dc1ab1f19f9f003578486397a
Bekir Gülestan <gulestanbekir@gmail.com>
2026-05-11 22:43:06 +0200

    add ui api host diffrence

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,6 +1,7 @@
 import posthog from 'posthog-js'
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_API_HOST,
+    ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     defaults: '2026-01-30'
 })
